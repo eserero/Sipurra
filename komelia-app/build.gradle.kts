6704fad0733d58dd3d18021408ab2026336affca
eserero <eserero@hotmail.com>
2026-04-14 12:46:07 +0300

    chore: release 1.9.0

--- komelia-app/build.gradle.kts
+++ komelia-app/build.gradle.kts
@@ -113,7 +113,7 @@ android {
         applicationId = "io.github.snd_r.komelia"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 20
+        versionCode = 21
         versionName = libs.versions.app.version.get()
 
         val enableSelfUpdates = when (androidVariant) {
--- komelia-domain/core/src/commonMain/kotlin/snd/komelia/updates/AppVersion.kt
+++ komelia-domain/core/src/commonMain/kotlin/snd/komelia/updates/AppVersion.kt
@@ -17,7 +17,7 @@ data class AppVersion(
 ) : Comparable<AppVersion> {
 
     companion object {
-        val current = AppVersion(1, 8, 0)
+        val current = AppVersion(1, 9, 0)
 
         fun fromString(value: String): AppVersion {
             val sanitized = value.removePrefix("v")
